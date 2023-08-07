6e3240dad9d00b210a36c10b3359b45d4c088489
wu <yjrhgvbn@gmail.com>
2023-08-07 16:49:57 +0800

    ✨ feat: 按键调整

--- src/ruler/punctuation.ts
+++ src/ruler/punctuation.ts
@@ -1,22 +1,25 @@
-import { map, mapSimultaneous, ToKeyCode, FromKeyCode, ComplexModifications } from "karabiner.ts";
-import { withHyper } from "../utils";
-import { describe } from "node:test";
+import { map, ToKeyCode, FromKeyCode } from "karabiner.ts";
 import { PUNCTUATION_KEY, PUNCTUATION_KEY_ID } from "./prefix";
 
 export function punctuationMap() {
   return [
-    punctuationKeyBind("spacebar", [PUNCTUATION_KEY, "spacebar"], `空格不做映射`, false),
-    punctuationKeyBind("return_or_enter", [PUNCTUATION_KEY, "return_or_enter"], "回车不做映射", false),
+    // punctuationKeyBind("spacebar", [PUNCTUATION_KEY, "spacebar"], `空格不做映射`, false),
+    punctuationKeyBind(
+      "return_or_enter",
+      [PUNCTUATION_KEY, "return_or_enter"],
+      "回车不做映射",
+      false
+    ),
     punctuationKeyBind("tab", [PUNCTUATION_KEY, "tab"], "tab不做映射", false),
     punctuationKeyBind("q", "1", "感叹号 `!`"),
     punctuationKeyBind("w", "2", "at `@`"),
     punctuationKeyBind("e", "3", "井号 `#`"),
     punctuationKeyBind("r", "4", "美元符号 `$`"),
-    punctuationKeyBind("t", "6", "抑扬符号 `^`"),
-    punctuationKeyBind("y", "7", "与符号 `&`"),
-    punctuationKeyBind("u", "8", "星号 `*`"),
-    punctuationKeyBind("i", "hyphen", "减号 `-`", false),
-    punctuationKeyBind("o", "equal_sign", "加号 `+`"),
+    punctuationKeyBind("t", "5", "百分号 `%`"),
+    punctuationKeyBind("y", "6", "抑扬符号 `^`"),
+    punctuationKeyBind("u", "7", "与符号 `&`"),
+    punctuationKeyBind("i", "8", "星号 `*`"),
+    punctuationKeyBind("o", "grave_accent_and_tilde", "重音符号 `", false),
 
     punctuationKeyBind("a", "9", "左括号 `(`"),
     punctuationKeyBind("s", "0", "右括号 `)`"),
@@ -26,12 +29,19 @@ export function punctuationMap() {
     punctuationKeyBind("h", "close_bracket", "右大括号 `}`"),
 
     punctuationKeyBind("j", "equal_sign", "等于 `=`", false),
-    punctuationKeyBind("k", "hyphen", "下划线 `_`"),
-    punctuationKeyBind("l", "grave_accent_and_tilde", "重音符号 `", false),
+    punctuationKeyBind("k", "backslash", "左斜杆 `\\`", false),
+    punctuationKeyBind("l", "backslash", "竖杠 `|`"),
 
-    punctuationKeyBind("v", "slash", "斜杆 `//`", false),
-    punctuationKeyBind("b", "backslash", "竖杠 `|`"),
-    punctuationKeyBind("m", "slash", "问号 `?`"),
+    punctuationKeyBind("c", "slash", "斜杆 `//`", false),
+    punctuationKeyBind("v", "slash", "问号 `?`"),
+    punctuationKeyBind("b", "hyphen", "减号 `-`", false),
+    punctuationKeyBind("n", "equal_sign", "加号 `+`"),
+    punctuationKeyBind("m", "hyphen", "下划线 `_`"),
+    // punctuationKeyBind("m", "slash", "问号 `?`"),
+    // punctuationKeyBind("k", "hyphen", "下划线 `_`"),
+
+    // punctuationKeyBind("i", "hyphen", "减号 `-`", false),
+    // punctuationKeyBind("o", "equal_sign", "加号 `+`"),
 
     // punctuationKeyBind("a", "hyphen", "减号 -", false),
     // punctuationKeyBind("s", "equal_sign", "加号 +"),
@@ -40,7 +50,12 @@ export function punctuationMap() {
   ];
 }
 
-function punctuationKeyBind(fromKey: FromKeyCode, toKey: ToKeyCode | ToKeyCode[], dec: string = "", withShift = true) {
+function punctuationKeyBind(
+  fromKey: FromKeyCode,
+  toKey: ToKeyCode | ToKeyCode[],
+  dec: string = "",
+  withShift = true
+) {
   const res = map({ key_code: fromKey }).description(dec).condition({
     name: PUNCTUATION_KEY_ID,
     value: 1,
@@ -51,7 +66,10 @@ function punctuationKeyBind(fromKey: FromKeyCode, toKey: ToKeyCode | ToKeyCode[]
       res.to({ key_code: key });
     });
   } else {
-    res.to({ key_code: toKey, ...(withShift ? { modifiers: ["left_shift"] } : {}) });
+    res.to({
+      key_code: toKey,
+      ...(withShift ? { modifiers: ["left_shift"] } : {}),
+    });
   }
   return res;
 }
